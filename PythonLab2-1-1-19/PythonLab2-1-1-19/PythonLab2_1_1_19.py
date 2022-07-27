ba569a480b6bfaebcb34b11724b2747fb0c62ddf
Harvey1968 <108738675+Harvey1968@users.noreply.github.com>
2022-07-27 11:34:18 +0100

    Add files via upload

---
+++ PythonLab2-1-1-19/PythonLab2-1-1-19/PythonLab2_1_1_19.py
@@ -0,0 +1,16 @@
+#Oridginal
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+print()
+
+#Modified ex1
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****", end="\n\n")
+
+#Modified ex2
+print("    *","   * *","  *   *"," *     *","***   ***","  *   *","  *   *","  *****", sep="\n", end="\n\n")
